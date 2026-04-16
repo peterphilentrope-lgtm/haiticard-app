a26e21d1bbf9f94535e279633f857fb9421c50d0
peterphilentrope-lgtm <peterphilentrope@gmail.com>
2026-04-16 15:00:59 -0400

    Create WithdrawPage.jsx

---
+++ src/src/pages/WithdrawPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function WithdrawPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Withdraw</h1>
+      <p>Withdraw your funds here.</p>
+    </div>
+  );
+}
