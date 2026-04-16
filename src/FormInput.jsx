b99630a4b19747e87b1e1436458bea24363fda37
peterphilentrope-lgtm <peterphilentrope@gmail.com>
2026-04-16 14:30:59 -0400

    Update FormInput.jsx

--- src/FormInput.jsx
+++ src/FormInput.jsx
@@ -1,1 +1,20 @@
+import React, { memo } from 'react';
 
+const FormInput = memo(({ label, labelEn, type, value, onChange, placeholder, required = true }) => {
+  return (
+    <div>
+      <label>
+        {label} {labelEn && ` / ${labelEn}`} {required && '*'}
+      </label>
+
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+});
+
+export default FormInput;
