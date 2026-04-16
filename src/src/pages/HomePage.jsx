68f3b11eafcd18a5e7274ad533e8a1d492358bbb
peterphilentrope-lgtm <peterphilentrope@gmail.com>
2026-04-16 14:40:05 -0400

    Create HomePage.jsx

---
+++ src/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>HaitiCard</h1>
+      <p>Welcome to your app</p>
+    </div>
+  );
+}
