0f00cd7a9de3221573309d24fe8315a755e43d0e
peterphilentrope-lgtm <peterphilentrope@gmail.com>
2026-04-16 14:57:54 -0400

    Create ReceiveMoneyPage.jsx

---
+++ src/src/pages/ReceiveMoneyPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ReceiveMoneyPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Receive Money</h1>
+      <p>Receive international payments here.</p>
+    </div>
+  );
+}
