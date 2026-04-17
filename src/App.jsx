53fa1a9164e9152802904a993b4340835a536ef6
peterphilentrope-lgtm <peterphilentrope@gmail.com>
2026-04-16 22:55:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HomePage from './pages/HomePage';
+import ReceiveMoneyPage from './pages/ReceiveMoneyPage';
 
 export default function App() {
   return (
     <div>
       <HomePage />
+      <ReceiveMoneyPage />
     </div>
   );
 }
